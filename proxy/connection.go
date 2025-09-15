424e8d45cb10f1d91001ad83d89d03abc03ac565
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-15 17:30:40 +0530

    implemnt handelconn func

--- proxy/connection.go
+++ proxy/connection.go
@@ -3,13 +3,59 @@ package proxy
 import (
 	"fmt"
 	"go-proxy-server/configuration"
+	"io"
 	"net"
+	"strconv"
 )
 
-func handleConnection(user net.Conn, config configuration.Socket) {
+func HandleConnection(user net.Conn, config configuration.Socket) {
+	defer user.Close()
 
 	userAddress := user.RemoteAddr().(*net.TCPAddr).IP.String()
+	fmt.Println("new connection from", userAddress)
 
-	fmt.Println("new connection %s", userAddress)
+	// socks5 handshake
+	if err := HandleHandshake(user); err != nil {
+		fmt.Println("handshake failed:", err)
+		return
+	}
 
+	// authentication
+	if err := UserAuthentication(user, config); err != nil {
+		fmt.Println("authentication failed:", err)
+		return
+	}
+
+	// read request
+	targetAddress, targetPort, err := ReadRequest(user)
+	if err != nil {
+		fmt.Println("request parsing failed:", err)
+		return
+	}
+
+	//connect target server
+	target, err := net.Dial("tcp", net.JoinHostPort(targetAddress, strconv.Itoa(targetPort)))
+	if err != nil {
+		fmt.Printf("failed to connect target server %s:%d: %v\n", targetAddress, targetPort, err)
+		SendResponse(user, 0x04, targetAddress, targetPort) // host unreachable
+		return
+	}
+	defer target.Close()
+
+	// send success
+	if err := SendResponse(user, 0x00, targetAddress, targetPort); err != nil {
+		fmt.Println("failed to send success response:", err)
+		return
+	}
+
+	fmt.Printf("forwarding %s -> %s:%d\n", userAddress, targetAddress, targetPort)
+
+	// bidirectional copy
+	go func() {
+		defer user.Close()
+		io.Copy(target, user)
+	}()
+	io.Copy(user, target)
+
+	fmt.Printf("connection closed %s -> %s:%d\n", userAddress, targetAddress, targetPort)
 }
