ffe885c2af4bb9cade5b038d7c1c54f28918a326
thushan-123 <thushanmadusanka456@gmail.com>
2025-09-15 14:08:47 +0530

    create socket configuration

--- main.go
+++ main.go
@@ -1,10 +1,30 @@
 package main
 
+import "os"
 
+type Socket struct {
+	address string
+	username string
+	password string
+}
 
+func socketConfigeration () Socket {
+	port := os.Getenv("PROXY_PORT")
+
+	if port == "" { 
+		port = "1080"
+	}
+
+	return Socket{
+		address: ":" + port,
+		username: "admin",
+		password: "password",
+	}
+
+}
 
 func main() {
 
 
-	
+
 }
